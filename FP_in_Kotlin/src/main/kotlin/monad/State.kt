ef92e80ff2a5adb3a4db1d9a33e0add65ef27ed3
ywchae1209 <path0s@naver.com>
2023-08-22 19:26:28 +0900

    State Monad ; Random/ CoinMachine

--- FP_in_Kotlin/src/main/kotlin/monad/State.kt
+++ FP_in_Kotlin/src/main/kotlin/monad/State.kt
@@ -8,6 +8,8 @@ import collections.SList.Companion.map
 import monad.State.Companion.sequence
 
 import monad.CoinMachine.simulateMachine
+import monad.Random.random_Ints
+import monad.State.Companion.get
 
 data class State<S, out A>(val run: (S) -> Pair<A, S>) {
 
@@ -46,10 +48,34 @@ data class State<S, out A>(val run: (S) -> Pair<A, S>) {
             val (b, s2) = sb.run(s)
             f(a,b) to s1        // which one to choose ?
         }
+        fun <S> get(): State<S,S> = State { s -> s to s }
+        fun <S> set(s: S): State<S,Unit> = State { _ -> Unit to s }
+        fun <S> modify(f: (S) -> S): State<S, Unit> = get<S>().flatMap { s -> set(s) }
     }
-    fun <S> get(): State<S,S> = State { s -> s to s }
-    fun <S> set(s: S): State<S,Unit> = State { _ -> Unit to s }
-    fun <S> modify(f: (S) -> S): State<S, Unit> = get<S>().flatMap { s -> set(s) }
+}
+
+object Random {
+
+    interface RNG {
+        fun nextInt() : Pair<Int, RNG>
+    }
+
+    data class SimpleRNG(val seed: Long) : RNG {
+        override fun nextInt(): Pair<Int, RNG> {
+            val newSeed = (seed * 0x5DEECE66DL + 0xBL) and 0xFFFFFFFFFFFFL
+            val nextRNG = SimpleRNG(newSeed)
+            val n = (newSeed ushr 16).toInt()
+            return n to nextRNG
+        }
+    }
+
+    private fun gen_Int() : State<RNG, Int> = State{ it.nextInt() }
+    private fun gen_ints(n: Int) = SList.fill(n, Unit).map{ _ -> gen_Int()}.sequence()
+
+    fun random_Ints( n: Int, seed: Long = 12345L) =
+        gen_ints(n).run(SimpleRNG(seed)).first
+
+
 }
 
 object CoinMachine {
@@ -86,20 +112,24 @@ object CoinMachine {
         inputs
             .map{ update(it)}
             .sequence()
+            .flatMap{ _ -> get<Machine>()}
+            .map{ it.coins to it.candies}
+
 }
 
 /// CoinMachine example
 fun main(args: Array<String>) {
 
-    val ints = SList.of(1,2,3,5,6,7,8,9,10)
+    val ints = random_Ints(100)
 
     val inputs = ints.map{ if(0 == it % 2) CoinMachine.Coin else CoinMachine.Turn }
     println(inputs)
 
-    val init = CoinMachine.Machine( true, 100, 100)
+    val init = CoinMachine.Machine( true, 30, 30)
     println(init)
 
     val result = simulateMachine(inputs).run(init)
-    println(result.first)
-    println(result.second)
+    println("(coin, candy)   = ${result.first}")
+    println("(machine state) = ${result.second}")
+
 }
